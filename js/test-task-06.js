c0c16e08ffd507ddeface180b72e3683b382c1e5
Kirill Miklashevich <Dizardmk@gmail.com>
2020-12-05 15:04:19 +0200

    Create test-task-06.js

---
+++ js/test-task-06.js
@@ -0,0 +1,33 @@
+let total = 0;
+
+while (true) {
+  let input = prompt("Введите число");
+
+  if (!input) {
+    console.log("Отменено пользователем");
+    break;
+  }
+
+  input = Number(input);
+
+  if (Number.isNaN(input)) {
+    alert("Вы ввели не число, повторите попытку");
+    continue;
+  }
+
+  total += input;
+}
+alert(`Общая сумма чисел: ${total}`);
+
+// let total = 0;
+// do {
+//   let input = prompt("Введите число");
+//   if (!input) {
+//     alert("Отменено");
+//     break;
+//   }
+
+//   input = Number(input);
+//   total += input;
+// } while (true);
+// alert(`Общая сумма чисел: ${total}`);
