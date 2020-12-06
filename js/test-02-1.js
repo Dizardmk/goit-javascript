da477480522702c51ca0730b61ca4ee55b24ef4f
Kirill Miklashevich <Dizardmk@gmail.com>
2020-12-06 20:31:50 +0200

    Delete test-02-1.js

--- js/test-02-1.js
+++
@@ -1,8 +0,0 @@
-let value = +prompt('Введите число');
-// value = Number(value);
-
-if (Number.isNaN(value)) { console.log('Вы ввели не число'); }
-else {
-    if (value % 2 === 0) { console.log(`Число ${value} - четное`); }
-    else {console.log(`Число ${value} - НЕчетное`)}
-}
