9366d8c211d037eb0cc480f0f2f4fe38438a0d2a
Dizardmk <Dizardmk@gmail.com>
2021-02-21 15:05:27 +0200

    update2

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -8,11 +8,11 @@ const refs = {
   themeToggle: document.querySelector('#theme-switch-toggle'),
 };
 
+refs.body.classList.add(Theme.LIGHT);
 if (localStorage.getItem('dark-theme')) {
-  refs.themeToggle.checked = true;
+  refs.body.classList.remove(Theme.LIGHT);
   refs.body.classList.add(Theme.DARK);
-} else if (localStorage.getItem('light-theme')) {
-  refs.body.classList.add(Theme.LIGHT);
+  refs.themeToggle.checked = true;
 }
 
 refs.themeToggle.addEventListener('change', event => onThemeSwitcher());
